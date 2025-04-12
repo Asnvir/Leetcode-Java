486729f333169c62d7dc847ca07e524d5e2b0d20
Artur Kopytin <asnvir@gmail.com>
2025-04-12 18:41:34 +0300

    0392-IS-SUBSEQUENCE Stats: Time: 2 ms (66.11%), Space: 41.4 MB (73.2%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isSubsequence(String sub, String s) {
-        if (sub.length() == 0) return true;
-        if (sub.length() > s.length()) return false;
-
+        // If 'sub' is empty, it is always a subsequence of 's', even if 's' is empty
+        if (sub.isEmpty()) return true;
+        
         int subIndx = 0;
-
-        for( int sIndx = 0; sIndx < s.length(); sIndx++){
-            if(s.charAt(sIndx) == sub.charAt(subIndx)){
+        
+        for (int sIndx = 0; sIndx < s.length(); sIndx++) {
+            if (subIndx < sub.length() && s.charAt(sIndx) == sub.charAt(subIndx)) {
                 subIndx++;
             }
             if (subIndx == sub.length()) return true;
         }
-
-        return subIndx == sub.length();
+        
+        return false;
     }
 }
